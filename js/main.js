d855df588a7cf6296d45925ebb4f1cca19e1e825
LinkinYoung <1143052146@qq.com>
2017-02-07 21:32:47 +0800

    加载时nav增加提示条

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 var nav = {
+    _progress: $('#menu-progress'),
     init: function() {
         $(".button-collapse").sideNav();
+    },
+    update: function() {
         if(app.isAdmin() == true) {
             $('.menu-admin').show();
         }
+        this.hideProgress();
+    },
+    showProgress: function() {
+        this._progress.show();
+    },
+    hideProgress: function() {
+        this._progress.hide();
     }
 }
 
 var app = {
-    _admin: false,
+    _admin: true,
     isAdmin: function() {
         return this._admin;
     },
@@ -17,6 +27,12 @@ var app = {
     init: function() {
         //各Frame／类对应一个html和一个js，各类／Frame负责绑定自己页面上的元素事件。
         nav.init();
+        nav.showProgress();
+
+        //检查登陆之后更新nav
+        setTimeout(function() {
+            nav.update();
+        }, 1000);
     },
 
     //一下几个方法用于引入／删除一个Frame／类，表现为替换_mainFrame的元素。这几个方法可能会被其他类／对象调用。
