831c7c33b6c232b76e0be579e0b562d46268e3c6
LinkinYoung <1143052146@qq.com>
2017-02-07 21:23:35 +0800

    Nav类 显示管理员可见的项目

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
+var nav = {
+    init: function() {
+        $(".button-collapse").sideNav();
+        if(app.isAdmin() == true) {
+            $('.menu-admin').show();
+        }
+    }
+}
+
 var app = {
-    _isAdmin: false,
+    _admin: false,
+    isAdmin: function() {
+        return this._admin;
+    },
     _mainFrame: $('main'),
 
     init: function() {
         //各Frame／类对应一个html和一个js，各类／Frame负责绑定自己页面上的元素事件。
-        $(".button-collapse").sideNav();
-        this.getSearchFrame();
+        nav.init();
     },
 
     //一下几个方法用于引入／删除一个Frame／类，表现为替换_mainFrame的元素。这几个方法可能会被其他类／对象调用。
