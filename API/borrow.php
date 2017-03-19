0a24282c55cd24c4746a9646b64193c280b213e0
LinkinYoung <1143052146@qq.com>
2017-03-19 15:18:51 +0800

    完成listMy接口，borrow数据表增加isbn栏

--- API/borrow.php
+++ API/borrow.php
@@ -44,8 +44,18 @@
             throw new Exception('Out of Stock.');
         }
 
-        $stmt = $dbh->prepare("INSERT INTO `borrow` (`bookUID`, `userID`, `name`, `date`) VALUES (:bookUID, :userID, :name, CURRENT_DATE)");
+        $stmt = $dbh->prepare("SELECT * FROM `borrow` WHERE `isbn` = :isbn AND `userID` = :userID AND returnDate = 0");
+        $stmt->bindParam(":isbn", $isbn);
+        $stmt->bindParam(":userID", $_SESSION['usrid']);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            throw new Exception('Out of Stock.');
+        }
+
+        $stmt = $dbh->prepare("INSERT INTO `borrow` (`bookUID`, `isbn`, `userID`, `name`, `date`) VALUES (:bookUID, :isbn, :userID, :name, CURRENT_DATE)");
         $stmt->bindParam(":bookUID", $_GET['bookUID']);
+        $stmt->bindParam(":isbn", $isbn);
         $stmt->bindParam(":userID", $_SESSION['usrid']);
         $stmt->bindParam(":name", $_SESSION['name']);
         $stmt->execute();
