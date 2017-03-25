32139a607df7ae9fe9d6797ca022a0034bbbd79c
LinkinYoung <1143052146@qq.com>
2017-03-25 10:11:23 +0800

    修复每天统计数据清零的问题

--- API/initDay.php
+++ API/initDay.php
@@ -1,5 +1,5 @@
 <?php
     include_once('connectDB.php');
     
-    $dbh->exec("INSERT INTO `static` (`date`, `amount`) VALUES (CURRENT_DATE, '0')");
+    $dbh->exec("INSERT INTO static SELECT (CURRENT_DATE) as date,  amount FROM static WHERE date = date_add(CURRENT_DATE, interval -1 day)");
 ?>
